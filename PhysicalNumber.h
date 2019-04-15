1873913b3f56242a38081c720375b601c14be98a
Amir Adar <44618095+amiradar12345@users.noreply.github.com>
2019-04-15 19:13:16 +0300

    Add files via upload

---
+++ PhysicalNumber.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include "Unit.h"
+
+using namespace std;
+
+namespace ariel{
+class PhysicalNumber{
+ private:
+     Unit unit;
+     double n;
+  public:
+     PhysicalNumber(double n,ariel::Unit unit);
+     PhysicalNumber& operator ++();
+     PhysicalNumber& operator --();
+     PhysicalNumber operator ++(int);
+     PhysicalNumber operator --( int);
+     double cast_it_to(ariel::Unit to_type) const;
+     string get_type(const PhysicalNumber& l);
+     Unit get_unit() const{return unit;} 
+     double get_number() const{return n;} 
+     void set_number(double number){n=number;}
+     string get_string_unit(const PhysicalNumber& l);
+  
+  friend PhysicalNumber operator + (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend PhysicalNumber operator += (PhysicalNumber& l,const PhysicalNumber& r);
+  friend PhysicalNumber operator - (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend PhysicalNumber operator - (const PhysicalNumber& l);
+  friend PhysicalNumber operator + (const PhysicalNumber& l);
+  friend PhysicalNumber operator -= (PhysicalNumber& l,const PhysicalNumber& r);
+  friend istream& operator >> (istream & is,PhysicalNumber& f);
+  friend ostream& operator <<(ostream & os,const PhysicalNumber& f);
+  friend bool operator > (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend bool operator < (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend bool operator >= (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend bool operator <= (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend bool operator == (const PhysicalNumber& l,const PhysicalNumber& r);
+  friend bool operator != (const PhysicalNumber& l,const PhysicalNumber& r);
+  
+};
+};
